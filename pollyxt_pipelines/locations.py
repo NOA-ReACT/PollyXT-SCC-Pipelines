9978ea85144ce38fa3dc67c9554629d118f2d20c
Thanasis Georgiou <ageorgiou@noa.gr>
2020-11-05 18:57:16 +0200

    Add documentation to locations

--- pollyxt_pipelines/locations.py
+++ pollyxt_pipelines/locations.py
@@ -9,13 +9,20 @@ class Location(NamedTuple):
     '''
     Represents a physical location of PollyXT installation.
     '''
+
+    '''Location friendly name'''
     name: str
+    '''How are the WRF profile names prefixed'''
     profile_name: str
+    '''SCC Station code'''
     scc_code: str
+    '''Latitude of station'''
     lat: float
+    '''Longitude of station'''
     lon: float
 
 
+'''Location at PANGEA observatory - Antikythera'''
 LOCATION_ANTIKYTHERA = Location(
     name="Antikythera",
     profile_name="ANTIKYTHERA",
@@ -24,8 +31,10 @@ LOCATION_ANTIKYTHERA = Location(
     lon=35.8612
 )
 
+'''Location at Finokalia (UoC)'''
 LOCATION_FINOKALIA = Location(
     name="Finokalia",
+    profile_name="FINOKALIA",
     scc_code='fik',
     lat=25.6698,
     lon=35.3377
