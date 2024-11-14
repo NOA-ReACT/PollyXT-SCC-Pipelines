create-scc: Fix calculation of `Raw_Data_Start_Time` in SCC files

pollyxt_pipelines/polly_to_scc/pollyxt.py
@@ -242,9 +242,8 @@ class PollyXTRepository:
         pollyxt_file.raw_signal_swap = np.concatenate(
             [x.raw_signal_swap for x in polly_files]
         )
-        pollyxt_file.measurement_time = np.concatenate(
-            [x.measurement_time for x in polly_files]
-        )
+        t_len = pollyxt_file.raw_signal.shape[0]
+        pollyxt_file.measurement_time = np.arange(0, t_len * 30, 30)
         pollyxt_file.measurement_shots = np.concatenate(
             [x.measurement_shots for x in polly_files]
         )

pollyxt_pipelines/polly_to_scc/scc_netcdf.py
@@ -174,15 +174,8 @@ def create_scc_netcdf(
     lr_input = nc.createVariable("LR_Input", "i4", dimensions=("channels"), zlib=True)
 
     # Fill Variables with Data. (mandatory)
-    print(pf.measurement_time[:, 1])
-    print(pf.calibration_mask)
-
-    raw_data_start_time[:] = (
-        pf.measurement_time[~pf.calibration_mask, 1] - pf.measurement_time[0, 1]
-    )
-    raw_data_stop_time[:] = (
-        pf.measurement_time[~pf.calibration_mask, 1] - pf.measurement_time[0, 1]
-    ) + 30
+    raw_data_start_time[:] = pf.measurement_time[~pf.calibration_mask]
+    raw_data_stop_time[:] = (pf.measurement_time[~pf.calibration_mask]) + 30
     raw_lidar_data[:] = pf.raw_signal_swap[~pf.calibration_mask]
     id_timescale[:] = np.zeros(np.size(pf.raw_signal[~pf.calibration_mask], axis=2))
     laser_pointing_angle[:] = int(pf.zenith_angle.item(0))